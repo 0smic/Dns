9d5c16b7cb7ca8e75850e9f055d3a1b65351b418
Gokul <146622216+0smic@users.noreply.github.com>
2026-04-25 04:45:41 +0530

    Enhance packet.h with IP details and packet pointer structures
    Added new structures for IP details and packet pointers, updated DNS tail structure.

--- dns server/packet.h
+++ dns server/packet.h
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
 
 
 #define packed __attribute__((packed))
@@ -21,6 +22,24 @@ struct packed ip{
 
 };
 
+struct packed ip_details{
+    uint8_t version;
+    uint8_t ihl;
+    uint8_t dscp;
+    uint8_t ecn;
+    uint16_t total_length;
+    uint16_t identification;
+    uint8_t flag;
+    uint16_t fragment;
+    uint8_t timetolive;
+    uint8_t protocol;
+    uint16_t check_sum;
+    uint32_t source;
+    uint32_t destination;
+};
+
+
+
 struct packed eth{
     uint8_t dest_mac[6];
     uint8_t src_mac[6];
@@ -39,7 +58,7 @@ struct packed dns_header{
     }packed num;
 };
 
-struct packed dns_tail{
+struct packed dns_question_tail{
     uint16_t qtype;
     uint16_t qclass;
 };
@@ -52,11 +71,12 @@ struct packed udp{
 
 };
 
-struct packed packet{
-    struct eth;
-    struct ip;
-    struct udp;
-    struct dns;
+struct packed packet_ptr{
+    struct eth *eth;
+    struct ip *ip;
+    struct udp *udp;
+    struct dns_header *dns_header;
+    struct dns_question_tail *tail;
 };
 
 
@@ -65,9 +85,24 @@ enum {
     ETH_SIZE = sizeof(struct eth),
     IP_SIZE  = sizeof(struct ip),
     UDP_SIZE = sizeof(struct udp),
+    DNS_HEAD_SIZE = sizeof(struct dns_header),
 
     ETH_OFFSET = 0,
     IP_OFFSET  = ETH_OFFSET + ETH_SIZE,
     UDP_OFFSET = IP_OFFSET + IP_SIZE,
-    DNS_OFFSET = UDP_OFFSET + UDP_SIZE
+    DNS_OFFSET = UDP_OFFSET + UDP_SIZE,
+    DATA_OFFSET = DNS_HEAD_SIZE + DNS_OFFSET,
+
+    QUESTION_IPV4 = 0x01,
+    QUESTION_INTERNET = 0X01,
+
+    ETH_IPV4 = 0x0800
 };
+
+
+struct packet_ptr *packet_init(uint8_t[]);
+int big_or_little();
+void extract(struct ip*);
+uint16_t conv16(uint16_t);
+uint32_t conv32(uint32_t);
+int get_endianness();
