6d41fbc7078cd5847dbff7e3c032a9b04d606a47
Gokul <146622216+0smic@users.noreply.github.com>
2026-04-25 04:45:03 +0530

    Refactor DNS packet handling and initialization

--- dns server/main.c
+++ dns server/main.c
@@ -5,22 +5,58 @@ uint16_t conv16(uint16_t in);
 int big_or_little();
 int get_endianness();
 
-uint8_t buffer[1500];
+uint8_t raw_buffer[1500];
 
-struct packed packet_ptr{
-    struct eth *eth;
-    struct ip *ip;
-    struct udp *udp;
-    struct dns_header *dns_header;
+struct ip_details dflt_ip = {
+    .version = 4,
+    .ihl = 5,
+    .dscp = 0,
+    .ecn = 0
+
+    
 };
 
+
 struct packet_ptr *packet_init(uint8_t buffer[]){
-    struct packet_ptr *p;
+    struct packet_ptr *p = malloc(sizeof(struct packet_ptr));
     p->eth = (struct eth *)(buffer + ETH_OFFSET);
     p->ip = (struct ip *)(buffer + IP_OFFSET);
     p->udp = (struct udp *)(buffer + UDP_OFFSET);
     p->dns_header = (struct dns_header*) (buffer + DNS_OFFSET);
+
+    uint8_t *ptr = buffer + DATA_OFFSET; 
+
+    // change this to a func
+    *ptr++ = 6;
+    memcpy(ptr,"google",6);
+    ptr += 6;
+
+    *ptr++ = 3;
+    memcpy(ptr,"com",3);
+    ptr += 3;
+    *ptr++ = 0;
+    // end func
+
+    p->tail = (struct dns_question_tail *)ptr;
+    p->tail->qtype = conv16(QUESTION_IPV4);
+    p->tail->qclass = conv16(QUESTION_INTERNET); 
+    // fill the tail later
     return p;
+}
+
+void init_val_eth(struct packet_ptr *ptr){
+    // need to complete
+    struct eth *eth = ptr->eth;
+    eth->src_mac;
+    eth->dest_mac;
+    eth->eth_type = conv32(ETH_IPV4);
+}
+
+void init_val_ip(struct packet_ptr *ptr){
+    // need to complete
+    struct ip *ip = ptr->ip;
+    ip->version_IHL = (dflt_ip.version << 4) | dflt_ip.ihl;
+    ip->Dscp_ecn = (dflt_ip.dscp << 2) | dflt_ip.ecn;
 }
 
 
@@ -30,31 +66,32 @@ int big_or_little(){
 }
 
 void extract(struct ip *packet){
+    struct ip dup_packet = *packet;
     int little = get_endianness();
     uint8_t mask8;
-    uint8_t version = packet->version_IHL >> 4;
-    uint8_t ihl = packet->version_IHL & 0x0F;
-    uint8_t dscp = packet->Dscp_ecn >> 2;
+    uint8_t version = dup_packet.version_IHL >> 4;
+    uint8_t ihl = dup_packet.version_IHL & 0x0F;
+    uint8_t dscp = dup_packet.Dscp_ecn >> 2;
     mask8 = (1 << 2) - 1;
-    uint8_t ecn = packet->Dscp_ecn & mask8;
+    uint8_t ecn = dup_packet.Dscp_ecn & mask8;
     if(little){
-        packet->Total_length = conv16(packet->Total_length);
-        packet->identification = conv16(packet->identification);
-        packet->flag_fragment = conv16(packet->flag_fragment);
-        packet->check_sum = conv16(packet->check_sum);
-        packet->source = conv32(packet->source);
-        packet->destination = conv32(packet->destination);
+        dup_packet.Total_length = conv16(dup_packet.Total_length);
+        dup_packet.identification = conv16(dup_packet.identification);
+        dup_packet.flag_fragment = conv16(dup_packet.flag_fragment);
+        dup_packet.check_sum = conv16(dup_packet.check_sum);
+        dup_packet.source = conv32(dup_packet.source);
+        dup_packet.destination = conv32(dup_packet.destination);
     }
-    uint16_t total_length = packet->Total_length;
-    uint16_t identification = packet->identification;
-    uint8_t flag = (packet->flag_fragment >> 13) & 0x07;
+    uint16_t total_length = dup_packet.Total_length;
+    uint16_t identification = dup_packet.identification;
+    uint8_t flag = (dup_packet.flag_fragment >> 13) & 0x07;
     uint16_t mask16 = (1 << 13) - 1;
-    uint16_t fragment = packet->flag_fragment & mask16;
-    uint8_t timetolive = packet->timetolive;
-    uint8_t protocol = packet->protocol;
-    uint16_t check_sum = packet->check_sum;
-    uint32_t source = packet->source;
-    uint32_t destination = packet->destination;
+    uint16_t fragment = dup_packet.flag_fragment & mask16;
+    uint8_t timetolive = dup_packet.timetolive;
+    uint8_t protocol = dup_packet.protocol;
+    uint16_t check_sum = dup_packet.check_sum;
+    uint32_t source = dup_packet.source;
+    uint32_t destination = dup_packet.destination;
 }
 
 uint16_t conv16(uint16_t in){
